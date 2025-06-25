4d9d0d8793ca5f0d624257f53c08f61522917fed
shjee-afridi <shjeeafridi2004@gmail.com>
2025-06-25 20:11:08 +0500

    seo fixed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,17 @@
 // src/app/home/page.tsx
 import Home from '../page';
-export default Home;
+import React from 'react';
+import Head from 'next/head';
+
+export default function HomeWithCanonical(props: any) {
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href="https://hentaidiscord.com/home" />
+        <title>Hentai Discord Home</title>
+        <meta name="description" content="Welcome to the Hentai Discord Home page. Discover and share Discord servers! Unique content for /home." />
+      </Head>
+      <Home {...props} />
+    </>
+  );
+}
